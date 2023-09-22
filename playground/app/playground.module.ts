376bf8d40ff157bc5ca2a9a616782bb438f8c8d5
Raymond Gigliotti <ray@firestitch.com>
2023-09-22 11:49:28 -0400

    npm cleanup

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,54 +1,42 @@
-import { FsApiModule } from '@firestitch/api';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material/dialog';
+import { RouterModule, Routes } from '@angular/router';
+import { FsApiModule } from '@firestitch/api';
 
+import { FsDialogModule } from '@firestitch/dialog';
 import { FsExampleModule } from '@firestitch/example';
-import { FsMessageModule } from '@firestitch/message';
+import { FsFormModule } from '@firestitch/form';
 import { FsHtmlEditorModule, FsHtmlRendererModule } from '@firestitch/html-editor';
 import { FsLabelModule } from '@firestitch/label';
-import { FsFormModule } from '@firestitch/form';
+import { FsMessageModule } from '@firestitch/message';
 import { FsScrollbarModule } from '@firestitch/scrollbar';
-import { FsDialogModule } from '@firestitch/dialog';
-import { ToastrModule } from 'ngx-toastr';
 
-import { AppMaterialModule } from './material.module';
-import {
-  KitchenSinkComponent,
-  ExamplesComponent,
-  DialogComponent,
-} from './components';
 import { AppComponent } from './app.component';
+import {
+  DialogComponent,
+  ExamplesComponent,
+  KitchenSinkComponent,
+} from './components';
 import { KitchenSinkConfigureComponent } from './components/kitchen-sink-configure';
+import { AppMaterialModule } from './material.module';
 
 const routes: Routes = [
   { path: '', component: ExamplesComponent },
 ];
 
-// import 'froala-editor/js/plugins/align.min.js';
-// import 'froala-editor/js/plugins/colors.min.js';
-// import 'froala-editor/js/plugins/image.min.js';
-// import 'froala-editor/js/plugins/link.min.js';
-// import 'froala-editor/js/plugins/lists.min.js';
-// import 'froala-editor/js/plugins/paragraph_format.min.js';
-// import 'froala-editor/js/plugins/table.min.js';
-// import 'froala-editor/js/plugins/url.min.js';
-// import 'froala-editor/js/plugins/video.min.js';
-// import 'froala-editor/js/plugins/quote.min.js';
-
 
 @NgModule({
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     MatDialogModule,
     FsHtmlEditorModule.forRoot({
       activationKey: 'DUA2yE1G2E1A5B5B3pZGCTRSAPJWTLPLZHTQQe1JGZxC4B3A3C2B5A1C2E4F1A1==',
-      froalaConfig: { },
+      froalaConfig: {},
     }),
     FsHtmlRendererModule,
     BrowserAnimationsModule,
@@ -62,7 +50,6 @@ const routes: Routes = [
     FsExampleModule.forRoot(),
     FsApiModule.forRoot(),
     FsMessageModule.forRoot(),
-    ToastrModule.forRoot({ preventDuplicates: true }),
     RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
   ],
   declarations: [
