c40f2a21b834369471d2090bc839336761db82ba
Raymond Gigliotti <ray@firestitch.com>
2024-12-05 05:17:30 -0500

    SP-T3849_Froala:_Checkbox_-_do_not_strike_through_items

--- src/app/modules/html-editor/plugins/checklist.plugin.ts
+++ src/app/modules/html-editor/plugins/checklist.plugin.ts
@@ -5,7 +5,7 @@ import { PluginConfig } from './../interfaces/plugin-config';
 
 export class ChecklistPlugin extends Plugin {
 
-  public constructor(public config: PluginConfig = { name: 'checklist' }) {
+  constructor(public config: PluginConfig = { name: 'checklist' }) {
     super();
     this.config = {
       ...config,
@@ -16,9 +16,9 @@ export class ChecklistPlugin extends Plugin {
           svgPath: 'M8.4,10.3l-1.2,1.2l4,4L20,6.7l-1.2-1.2l-7.6,7.6L8.4,10.3z M18.2,18.2H5.8V5.8h8.9V4H5.8C4.8,4,4,4.8,4,5.8v12.4c0,1,0.8,1.8,1.8,1.8h12.4c1,0,1.8-0.8,1.8-1.8v-7.1h-1.8V18.2z',
           click: (editor) => {
             editor.html.insert('<ul class="checklist"><li></li></ul>');
-          }
-        }
-      ]
+          },
+        },
+      ],
     };
   }
 
